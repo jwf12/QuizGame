6289df404aabea2af29be0f2e6496e9ad7cb60e9
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2023-01-23 23:31:20 -0300

    add str method to model

--- polls/models.py
+++ polls/models.py
@@ -3,17 +3,29 @@ from django.db import models
 class Category(models.Model):
     category_text = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.category_text
+
 class Question(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null= True, related_name='questions') 
     question_text= models.CharField(max_length=200)
     pub_date= models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text= models.CharField(max_length=200)
     votes = models.IntegerField(default= 0)
 
+    def __str__(self):
+        return self.choice_text
+
 class Answer2(models.Model):
    choice= models.ForeignKey(Question, on_delete=models.CASCADE)
    answer_text= models.CharField(max_length=200)
+
+   def __str__(self):
+    return self.answer_text
     
