f225d5a7ce3377229c2e2fd00b80b71b94014385
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2023-01-23 22:44:51 -0300

    small change in url conf

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ app_name='polls'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('<int:pk>', views.QuestionDetailView.as_view(), name='numPreg'),
-    path('<question_id>/vote', views.vote, name='vote'),
+    path('<int:pk>/', views.QuestionDetailView.as_view(), name='numPreg'),
+    path('<question_id>/vote/', views.vote, name='vote'),
     # path('<question_id>/results', views.results, name='results'),
 ]
