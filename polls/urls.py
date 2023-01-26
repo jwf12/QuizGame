b018b6aba2128b31659a8734313efb4806423397
jwf12 <jwf_12@hotmail.com>
2023-01-25 22:52:01 -0300

    fixed(?

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name='polls'
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('<category_id>', views.detailView, name='numPreg'),
+    path('', views.index, name='index'),
+    path('<category_id>', views.detail_view, name='numPreg'),
     path('<question_id>/vote', views.vote, name='vote'),
     # path('<question_id>/results', views.results, name='results'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,38 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice, Answer2, Category
+from .models import Question, Choice, Category, Answer2
 from django.urls import reverse
 from django.views import generic
 
 
-def index (request):
+def index(request):
     contexto = {}
-    n = Category.objects.all()
-    contexto['all_category'] = n
+    all_category = Category.objects.all()
+    contexto['all_category'] = all_category
     return render(request, 'polls/index.html', contexto)
 
 
-
-# class DetailView(generic.DetailView):
-#     model = Question
-#     template_name = "polls/answ.html"
-#     context_object_name = 'latest_questions'
-
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/answ.html"
+    context_object_name = 'latest_questions'
 
 
-def detailView (request, category_id):
+def detail_view(request, category_id):
     contexto = {}
     selected_category = Category.objects.get(pk = category_id)
     contexto['selected_category'] = selected_category
-
-    c= Question.objects.filter(category = selected_category)
-    contexto['cat_questions'] = c
+    
+    cat_questions = Question.objects.filter(category = selected_category)
+    contexto['cat_questions'] = cat_questions
     return render(request, 'polls/answ.html', contexto)
 
 
-
-
-# def results (request, question_id):
+# def results(request, question_id):
 #     contexto = {}
 #     latest_questions = Question.objects.get(pk = question_id)
 #     contexto['latest_questions'] = latest_questions
@@ -42,23 +38,20 @@ def detailView (request, category_id):
 #     return render(request, 'polls/results.html', contexto)
     
 
-
 def vote(request, question_id):
     latest_questions = Question.objects.get(pk = question_id)
     right_ans = Answer2.objects.get(choice = latest_questions)
-    
-    try:
-        selected_choice= latest_questions.choice_set.get(pk= request.POST['choice'])
-        
 
+    try:
+        selected_choice= latest_questions.choice_set.get(pk = request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
         return render(request, "polls/answ.html", {
             'latest_questions': latest_questions,
             'error_massage': 'No elegiste una respuesta',
         })
 
-    else: 
+    else:
         if selected_choice.choice_text == right_ans.answer_text:
-            return render ( request, 'polls/results.html')
+            return redirect('polls:results', question_id=question_id)
         else:
-            return HttpResponseRedirect(reverse('polls:index'))
+            return redirect('polls:index')
